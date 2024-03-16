b5ce7ebe119fa454deab16148fe61a2f994c0930
amalvelloth <amalv414@gamil.com>
2024-03-17 03:52:18 +0530

    contents to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Home.css";
+import organiseSVG from "../assets/organise.svg";
+import eventSVG from "../assets/event.svg";
+import shakehandSVG from "../assets/shakehand.svg";
 
 function Home() {
   return (
     <div className="container-home">
-      <section>
+      <section className="home-section1">
         <div className="hero-home">
           <h1 className="mb-4">
             Welcome to <span className="bold-white">MIDNIGHT MAGIC</span> as the
             premier partner in creating unforgettable events.
           </h1>
           <p>
-            We specialize in crafting extraordinary events that leave a lasting
-            impression. From corporate gatherings to milestone celebrations,
-            we're here to turn your vision into reality. With our passion for
-            excellence and unwavering commitment to seamless execution, we're
-            your trusted partner in event management. Explore our services, meet
-            our team, and let's create unforgettable moments together.
+            With a passion for excellence and an unwavering commitment to
+            delivering seamless experiences, we are your go-to event management
+            experts. From corporate gatherings to milestone celebrations, we
+            specialize in crafting events that leave a lasting impression.
           </p>
-          <h1 className="mt-5 text-white">How it works ?</h1>
+          <h5 className="mt-5 text-white fw-light">How it works ?</h5>
           <div className="box">
             <div className="custom-row d-flex">
               <div className="container-box1 p-3 col-md-4 col-sm-6">
@@ -26,17 +27,58 @@ function Home() {
                   Share Ideas.
                   <br /> Our Event Organizers do for you
                 </h1>
+                <div className="d-flex">
+                  <button className="home-message-btn my-3">
+                    <a href="tel:8281578305" style={{textDecoration:'none'}}><span><h6 className="m-0">Call 8281578305</h6></span></a>
+                  </button>
+                </div>
               </div>
-              <div className="container-box2 d-flex justify-content-center p-3 col-md-8 col-sm-6">
+              <div className="container-box2 d-flex justify-content-center p-3 col-md-8 col">
                 <div className="row align-items-center">
-                  <div className="sm-box col-md-4">
-                    <h1 className="text-white">Icon1</h1>
+                  <div className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center">
+                    <div
+                      className="circle"
+                      style={{ backgroundColor: "#f3ce01" }}
+                    >
+                      <img
+                        className="svg"
+                        src={eventSVG}
+                        style={{ width: "50px" }}
+                        alt=""
+                      />
+                    </div>
+                    <span className="mt-2 text-center">
+                      <h6>We love to hear about event</h6>
+                    </span>{" "}
+                    {/* Short word */}
                   </div>
-                  <div className="sm-box col-md-4">
-                    <h1 className="text-white">Icon2</h1>
+                  <div className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center">
+                    <div
+                      className="circle"
+                      style={{ backgroundColor: "#DE287D" }}
+                    >
+                      <img
+                        src={shakehandSVG}
+                        style={{ width: "50px" }}
+                        alt=""
+                      />
+                    </div>
+                    <span className="mt-2 text-center">
+                      <h6>Plan and discuss with us</h6>
+                    </span>{" "}
+                    {/* Short word */}
                   </div>
-                  <div className="sm-box col-md-4">
-                    <h1 className="text-white">Icon3</h1>
+                  <div className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center">
+                    <div
+                      className="circle"
+                      style={{ backgroundColor: "#0172EF" }}
+                    >
+                      <img src={organiseSVG} style={{ width: "50px" }} alt="" />
+                    </div>
+                    <span className="mt-2 text-center">
+                      <h6>We organize Event Easily</h6>
+                    </span>{" "}
+                    {/* Short word */}
                   </div>
                 </div>
               </div>
