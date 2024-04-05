17f6f3d28cc5cb334b017ee345537492fecf9b41
amalvelloth <amalv414@gamil.com>
2024-04-05 17:45:04 +0530

    button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,16 @@ function Home() {
   return (
     <div className="container-home">
       <div className="inner">
-        <section className="home-section1" id="home">
+        <section
+          className="home-section1 align-items-center d-flex pt-0"
+          id="home"
+        >
           <div className="row">
             <div className="hero-home d-flex flex-column justify-content-center col-md-6">
               <h1 className="mb-5 ">
-                Welcome to <span className="animated-title">MIDNIGHT MAGIC</span> as
-                the premier partner in creating unforgettable events.
+                Welcome to{" "}
+                <span className="animated-title">MIDNIGHT MAGIC</span> as the
+                premier partner in creating unforgettable events.
               </h1>
               <p
                 style={{ zIndex: "1", lineHeight: "2" }}
@@ -37,10 +41,15 @@ function Home() {
                 celebrations, we specialize in crafting events that leave a
                 lasting impression.
               </p>
+
+              <button className="contact-now-btn mt-2"><h6 className="mb-0">CONTACT NOW</h6></button>
             </div>
 
-            <div className="card-container align-items-center justify-content-center col-md-6" data-aos="fade"
-              data-aos-duration="2000">
+            <div
+              className="card-container align-items-center justify-content-center col-md-6"
+              data-aos="fade"
+              data-aos-duration="2000"
+            >
               <div className="card">
                 <Carousel>
                   <Carousel.Item>
@@ -84,7 +93,10 @@ function Home() {
                   </h1>
                   <div className="d-flex">
                     <button className="home-message-btn my-3">
-                      <a href="tel:8281578305" style={{ textDecoration: "none" }}>
+                      <a
+                        href="tel:8281578305"
+                        style={{ textDecoration: "none" }}
+                      >
                         <span>
                           <h6 className="m-0">Call 8281578305</h6>
                         </span>
@@ -94,8 +106,11 @@ function Home() {
                 </div>
                 <div className="container-box2 d-flex justify-content-center p-3 col-md-8 col">
                   <div className="row align-items-center">
-                    <div className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center" data-aos="fade-up"
-                      data-aos-anchor-placement="center-bottom">
+                    <div
+                      className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center"
+                      data-aos="fade-up"
+                      data-aos-anchor-placement="center-bottom"
+                    >
                       <div
                         className="circle"
                         style={{ backgroundColor: "#f3ce01" }}
@@ -112,8 +127,11 @@ function Home() {
                       </span>{" "}
                       {/* Short word */}
                     </div>
-                    <div className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center" data-aos="fade-up"
-                      data-aos-anchor-placement="center-bottom">
+                    <div
+                      className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center"
+                      data-aos="fade-up"
+                      data-aos-anchor-placement="center-bottom"
+                    >
                       <div
                         className="circle"
                         style={{ backgroundColor: "#DE287D" }}
@@ -129,13 +147,20 @@ function Home() {
                       </span>{" "}
                       {/* Short word */}
                     </div>
-                    <div className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center" data-aos="fade-up"
-                      data-aos-anchor-placement="center-bottom">
+                    <div
+                      className="sm-box d-flex flex-column align-items-center col-md-4 col-sm-12 justify-content-center"
+                      data-aos="fade-up"
+                      data-aos-anchor-placement="center-bottom"
+                    >
                       <div
                         className="circle"
                         style={{ backgroundColor: "#0172EF" }}
                       >
-                        <img src={organiseSVG} style={{ width: "50px" }} alt="" />
+                        <img
+                          src={organiseSVG}
+                          style={{ width: "50px" }}
+                          alt=""
+                        />
                       </div>
                       <span className="mt-2 text-center">
                         <h6>We organize Event Easily</h6>
@@ -147,7 +172,12 @@ function Home() {
               </div>
             </div>
             <span>
-              <img src={balloons} style={{ width: "200px" }} alt="" data-aos="fade-up-right" />
+              <img
+                src={balloons}
+                style={{ width: "200px" }}
+                alt=""
+                data-aos="fade-up-right"
+              />
             </span>
           </div>
         </div>
